4d7ed3f3ad3182083094f0bf5de1540e296bf092
Chelowe4ok <chelowe4ok@gmai.com>
2018-05-25 00:47:03 +0300

    --amend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,23 +13,23 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        redirectTo: tabs[0].id,
+        redirectTo: tabs[0] ? tabs[0].id : '',
         pathMatch: 'full',
       },
       {
-        path: tabs[0].id,
+        path: tabs[0] ? tabs[0].id : '',
         loadChildren: "./components/tabs/+dummy-list/dummy-list.module#DummyListModule",
-        data: tabs[0]
+        data: tabs[0],
       },
       {
-        path: tabs[1].id,
+        path: tabs[1] ? tabs[1].id : '',
         loadChildren: "./components/tabs/+dummy-table/dummy-table.module#DummyTableModule",
         data: tabs[1]
       },
       {
-        path: tabs[2].id,
+        path: tabs[2] ? tabs[2].id : '',
         loadChildren: "./components/tabs/+dummy-chart/dummy-chart.module#DummyChartModule",
-        data: tabs[2]
+        data: tabs[2],
       }
     ]
   },
